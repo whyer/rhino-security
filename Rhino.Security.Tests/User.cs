7749c90ca6012e055811cf1ad2080be456d96780
Remco Ros <r.ros@mobielbereikbaar.nl>
2011-04-21 14:12:00 +0200

    equality functions on User

--- Rhino.Security.Tests/User.cs
+++ Rhino.Security.Tests/User.cs
@@ -14,5 +14,25 @@ namespace Rhino.Security.Tests
         {
             get { return new SecurityInfo(Name, Id); }
         }
+
+    	public virtual bool Equals(User other)
+    	{
+    		if (ReferenceEquals(null, other)) return false;
+    		if (ReferenceEquals(this, other)) return true;
+    		return other.Id == Id;
+    	}
+
+    	public override bool Equals(object obj)
+    	{
+    		if (ReferenceEquals(null, obj)) return false;
+    		if (ReferenceEquals(this, obj)) return true;
+    		if (obj.GetType() != typeof (User)) return false;
+    		return Equals((User) obj);
+    	}
+
+    	public override int GetHashCode()
+    	{
+    		return Id.GetHashCode();
+    	}
     }
 }
