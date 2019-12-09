af71d204265289e342ec0c3244c3fd6fff017376
Andreas Eriksson <andreas.eriksson@voyado.com>
2019-12-09 18:34:09 +0100

    Add possibility to easy change between SqlLite and Sql Server

--- Rhino.Security.Tests/DatabaseFixture.cs
+++ Rhino.Security.Tests/DatabaseFixture.cs
@@ -13,96 +13,106 @@ using Environment = NHibernate.Cfg.Environment;
 
 namespace Rhino.Security.Tests
 {
-	public abstract class DatabaseFixture : IDisposable
-	{
-		protected readonly ISessionFactory factory;
-		protected Account account;
-		protected IAuthorizationRepository authorizationRepository;
-		protected IAuthorizationService authorizationService;
-		protected IPermissionsService permissionService;
-		protected IPermissionsBuilderService permissionsBuilderService;
+    public abstract class DatabaseFixture : IDisposable
+    {
+        protected readonly ISessionFactory factory;
+        protected Account account;
+        protected IAuthorizationRepository authorizationRepository;
+        protected IAuthorizationService authorizationService;
+        protected IPermissionsService permissionService;
+        protected IPermissionsBuilderService permissionsBuilderService;
 
-		protected ISession session;
-		protected User user;
+        protected ISession session;
+        protected User user;
 
-		protected DatabaseFixture()
-		{
-			BeforeSetup();
+        public bool UseSqlDatabase;
 
-			SillyContainer.SessionProvider = (() => session);
-			var sillyContainer = new SillyContainer();
-			ServiceLocator.SetLocatorProvider(() => sillyContainer);
+        protected DatabaseFixture(bool useSqlDatabase = false)
+        {
+            UseSqlDatabase = useSqlDatabase;
 
-			Assert.NotNull(typeof (SQLiteConnection));
+            BeforeSetup();
 
-			Configuration cfg = new Configuration()
-				.SetProperty(Environment.ConnectionDriver, typeof(SQLite20Driver).AssemblyQualifiedName)
-				.SetProperty(Environment.Dialect, typeof(SQLiteDialect).AssemblyQualifiedName)
-				//.SetProperty(Environment.ConnectionDriver, typeof(Sql2008ClientDriver).AssemblyQualifiedName)
-				//.SetProperty(Environment.Dialect, typeof(MsSql2008Dialect).AssemblyQualifiedName)
-				.SetProperty(Environment.ConnectionString, ConnectionString)
-				//.SetProperty(Environment.ProxyFactoryFactoryClass, typeof(ProxyFactoryFactory).AssemblyQualifiedName)
+            SillyContainer.SessionProvider = (() => session);
+            var sillyContainer = new SillyContainer();
+            ServiceLocator.SetLocatorProvider(() => sillyContainer);
+
+            Assert.NotNull(typeof(SQLiteConnection));
+
+            var driverName = typeof(SQLite20Driver).AssemblyQualifiedName;
+            var dialectName = typeof(SQLiteDialect).AssemblyQualifiedName;
+
+            if (useSqlDatabase)
+            {
+                driverName = typeof(SqlClientDriver).AssemblyQualifiedName;
+                dialectName = typeof(MsSql2012Dialect).AssemblyQualifiedName;
+            }
+
+            Configuration cfg = new Configuration()
+                .SetProperty(Environment.ConnectionDriver, driverName)
+                .SetProperty(Environment.Dialect, dialectName)
+                .SetProperty(Environment.ConnectionString, ConnectionString)
 				.SetProperty(Environment.ReleaseConnections, "on_close")
-				.SetProperty(Environment.UseSecondLevelCache, "true")
-				.SetProperty(Environment.UseQueryCache, "true")
-				.SetProperty(Environment.CacheProvider, typeof (HashtableCacheProvider).AssemblyQualifiedName)
-				.AddAssembly(typeof (User).Assembly);
+                .SetProperty(Environment.UseSecondLevelCache, "true")
+                .SetProperty(Environment.UseQueryCache, "true")
+                .SetProperty(Environment.CacheProvider, typeof(HashtableCacheProvider).AssemblyQualifiedName)
+                .AddAssembly(typeof(User).Assembly);
 
-			Security.Configure<User>(cfg, SecurityTableStructure.Prefix);
+            Security.Configure<User>(cfg, SecurityTableStructure.Prefix);
 
-			factory = cfg.BuildSessionFactory();
+            factory = cfg.BuildSessionFactory();
 
-			session = factory.OpenSession();
+            session = factory.OpenSession();
 
-			new SchemaExport(cfg).Execute(true, true, false, session.Connection, null);
+            new SchemaExport(cfg).Execute(true, true, false, session.Connection, null);
 
-			session.BeginTransaction();
+            session.BeginTransaction();
 
-			SetupEntities();
+            SetupEntities();
 
-			session.Flush();
-		}
+            session.Flush();
+        }
 
-		public virtual string ConnectionString
-		{
-			get { return "Data Source=:memory:"; }
-		}
+        public virtual string ConnectionString
+        {
+            get { return UseSqlDatabase ? "Server=(local);Database=tests;Trusted_Connection=True;" : "Data Source=:memory:"; }
+        }
 
-		#region IDisposable Members
+        #region IDisposable Members
 
-		public virtual void Dispose()
-		{
-			if (session.Transaction.IsActive)
-				session.Transaction.Rollback();
-			session.Dispose();
-		}
+        public virtual void Dispose()
+        {
+            if (session.Transaction.IsActive)
+                session.Transaction.Rollback();
+            session.Dispose();
+        }
 
-		#endregion
+        #endregion
 
-		protected virtual void BeforeSetup()
-		{
-		}
+        protected virtual void BeforeSetup()
+        {
+        }
 
-		private void SetupEntities()
-		{
-			user = new User {Name = "Ayende"};
-			account = new Account {Name = "south sand"};
+        private void SetupEntities()
+        {
+            user = new User { Name = "Ayende" };
+            account = new Account { Name = "south sand" };
 
-			session.Save(user);
-			session.Save(account);
+            session.Save(user);
+            session.Save(account);
 
-			authorizationService = ServiceLocator.Current.GetInstance<IAuthorizationService>();
-			permissionService = ServiceLocator.Current.GetInstance<IPermissionsService>();
-			permissionsBuilderService = ServiceLocator.Current.GetInstance<IPermissionsBuilderService>();
-			authorizationRepository = ServiceLocator.Current.GetInstance<IAuthorizationRepository>();
+            authorizationService = ServiceLocator.Current.GetInstance<IAuthorizationService>();
+            permissionService = ServiceLocator.Current.GetInstance<IPermissionsService>();
+            permissionsBuilderService = ServiceLocator.Current.GetInstance<IPermissionsBuilderService>();
+            authorizationRepository = ServiceLocator.Current.GetInstance<IAuthorizationRepository>();
 
-			authorizationRepository.CreateUsersGroup("Administrators");
-			authorizationRepository.CreateEntitiesGroup("Important Accounts");
-			authorizationRepository.CreateOperation("/Account/Edit");
-			authorizationRepository.CreateOperation("/Account/Disable");
+            authorizationRepository.CreateUsersGroup("Administrators");
+            authorizationRepository.CreateEntitiesGroup("Important Accounts");
+            authorizationRepository.CreateOperation("/Account/Edit");
+            authorizationRepository.CreateOperation("/Account/Disable");
 
-			authorizationRepository.AssociateUserWith(user, "Administrators");
-			authorizationRepository.AssociateEntityWith(account, "Important Accounts");
-		}
-	}
+            authorizationRepository.AssociateUserWith(user, "Administrators");
+            authorizationRepository.AssociateEntityWith(account, "Important Accounts");
+        }
+    }
 }
